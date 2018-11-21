0f0a159207c6bedd8d6a06b0264f803d97c4b525
Alex <ezyah3@nottingham.ac.uk>
2018-11-21 15:39:35 +0000

    Initial commit

---
+++ app/src/main/java/com/example/poker/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.poker;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
