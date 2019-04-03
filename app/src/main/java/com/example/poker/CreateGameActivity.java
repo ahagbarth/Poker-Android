4ddc009eafef4ab4cc50e416143ce2ec6e43024c
Alex <ezyah3@nottingham.ac.uk>
2019-04-03 04:54:29 +0100

    Added database entries for creating tables and in  join table theres a recycler view for the list of tables, not clickable yet

--- app/src/main/java/com/example/poker/CreateGameActivity.java
+++ app/src/main/java/com/example/poker/CreateGameActivity.java
@@ -1,6 +1,7 @@
 package com.example.poker;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,12 +11,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class CreateGameActivity extends AppCompatActivity {
 
     EditText roomName;
     Button buttonCreateRoom;
 
     String RoomName;
+
+    private DatabaseReference mGamesDatabases;
 
 
 
@@ -31,13 +39,27 @@ public class CreateGameActivity extends AppCompatActivity {
         roomName = findViewById(R.id.roomName);
         buttonCreateRoom = findViewById(R.id.createRoom);
 
-
+        //Database Reference
+        mGamesDatabases = FirebaseDatabase.getInstance().getReference().child("Games");
 
 
 
         buttonCreateRoom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                mGamesDatabases.child(roomName.getText().toString()).child("gameName").setValue(roomName.getText().toString()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        mGamesDatabases.child(roomName.getText().toString()).child("numUsers").setValue(0).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                Toast.makeText(CreateGameActivity.this, "Game Created", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+                });
 
                 Intent intent = new Intent(CreateGameActivity.this, QuickmatchGameActivity.class);
                 intent.putExtra("RoomName", roomName.getText().toString());
--- app/src/main/java/com/example/poker/JoinGameActivity.java
+++ app/src/main/java/com/example/poker/JoinGameActivity.java
@@ -1,11 +1,24 @@
 package com.example.poker;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class JoinGameActivity extends AppCompatActivity {
+
+    private RecyclerView mGamesList;
+    private DatabaseReference mGamesDatabases;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +28,66 @@ public class JoinGameActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_join_game);
+
+        mGamesDatabases = FirebaseDatabase.getInstance().getReference().child("Games");
+
+        mGamesList = findViewById(R.id.gamesList);
+        mGamesList.setHasFixedSize(true);
+        mGamesList.setLayoutManager(new LinearLayoutManager(this));
+
+
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<Games, GamesViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Games, GamesViewHolder>(
+
+                Games.class,
+                R.layout.gamecard,
+                GamesViewHolder.class,
+                mGamesDatabases.orderByChild("gameName")
+
+        ) {
+            @Override
+            protected void populateViewHolder(GamesViewHolder viewHolder, Games model, int position) {
+                viewHolder.setName(model.getGameName());
+
+            }
+
+
+        };
+
+        mGamesList.setAdapter(firebaseRecyclerAdapter);
+
+
+
+    }
+
+    public static class GamesViewHolder extends RecyclerView.ViewHolder{
+
+        View mView;
+
+        public GamesViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            mView = itemView;
+        }
+        public void setName(String name){
+            TextView userNameView = mView.findViewById(R.id.gameName);
+            userNameView.setText(name);
+
+
+
+        }
+
+
+
+
+
+    }
+
+
 }
